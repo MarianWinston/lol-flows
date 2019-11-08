402b44fd836db04af613dbf8cecc245b89c266f7
Marian <marian.jitaru3@gmail.com>
2019-11-08 00:38:51 +0000

    made some data checks -> still not able to render li

--- lib/client/app/pages/Campions.jsx
+++ lib/client/app/pages/Campions.jsx
@@ -2,21 +2,43 @@ import React, { useState, useEffect } from "react";
 import "../styles/home.scss";
 
 function Champions() {
-  const [champions, setChampions] = useState({ championsId: 'loading'})
+  const [champions, setChampions] = useState([
+    { championId: "loading", championLevel: "loading" }
+  ]);
+  const [error, setError] = React.useState(null);
+
   async function getChampions() {
-    const champions = await fetch("api/champions");
-    const json = await champions.text();
-    console.log(json)
-    setChampions(json)
+    try {
+      const championsResponse = await fetch("api/champions");
+      const json = await championsResponse.json();
+      setChampions(json);
+    } catch {
+      setError(error);
+    }
+    console.log(champions);
   }
 
   useEffect(() => {
-    console.log(1)
-    getChampions()
-  }, [])
-  
+    getChampions();
+  }, []);
 
-  return <div>{champions.championsId}</div>;
+  console.log(champions);
+
+  if (champions.length > 1) {
+    return (
+      <ul>
+        {champions
+          .slice(0, 10)
+          .filter(Boolean)
+          .map((champ, idx) => {
+            console.log(champ); 
+            <li key={idx}>{champ}</li>; /* ===> BIG ISSUE HERE NO RENDERING LI(S)*/
+          })}
+      </ul>
+    );
+  }
+
+  return <p>Loading..</p>;
 }
 
 export default Champions;
